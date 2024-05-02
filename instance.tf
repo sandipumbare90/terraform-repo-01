87ee9f5243c2651a66c60791e1bbfc201f5a3925
sandipumbare90 <sandipumbare8@gmail>
2024-05-02 11:08:29 +0000

    new vm instance creation tf code

---
+++ instance.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "terraform-instance" {
+	name = "client-node-1"
+	machine_type = "e2-medium"
+	zone = "us-central-a"
+	boot_disk {
+          initialize_params {
+          image = "ubuntu-os-cloud/ubuntu-2204-lts"
+                             }
+                   }
+	network_interface {
+	  network = "projects/devops-engineer-123/global/networks/my-vpc-01"
+        access_config {
+                    }
+}
+
+
+}
