3c16b356c35da7f375686915bec28c68a45e217d
sandipumbare90 <sandipumbare8@gmail>
2024-05-03 09:47:45 +0000

    new label added

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "google_compute_instance" "terraform-instance" {
 	machine_type = "e2-medium"
 	zone = "us-central1-a"
 	labels = {
-	  team = "gcp"
+	  team = "devops"
 	}
 	boot_disk {
           initialize_params {
