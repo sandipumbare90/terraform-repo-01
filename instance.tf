b453c6377ba045613ce73e3b80bff83d4be79245
sandipumbare90 <sandipumbare8@gmail>
2024-05-03 08:53:03 +0000

    label added

--- instance.tf
+++ instance.tf
@@ -2,6 +2,9 @@ resource "google_compute_instance" "terraform-instance" {
 	name = "client-node-1"
 	machine_type = "e2-medium"
 	zone = "us-central-a"
+	labels = {
+	  team = "gcp"
+	}
 	boot_disk {
           initialize_params {
           image = "ubuntu-os-cloud/ubuntu-2204-lts"
