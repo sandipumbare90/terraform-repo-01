f22bfb9a2b925d9b75e6c98d7b115b7dc7e73610
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2024-05-17 15:59:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+backend "gcs" {
+    bucket = "gcs-bucket-for-backend-state-files"
+    prefix = "terraform-1/state"
+    impersonate_service_account = "jenkins-sa-cicd@devops-engineer-123.iam.gserviceaccount.com"
+}
+}
+
 provider "google" {
 	project = "devops-engineer-123"
 	region = "us-central1"
